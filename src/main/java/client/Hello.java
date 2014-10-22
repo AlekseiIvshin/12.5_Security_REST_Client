78db6f84f24e2e8a2c7a308207f7cdb438ad1839
Ivshin Aleksei <ivshinalekseim@mail.ru>
2014-10-22 17:35:55 +0400

    Fix closing stream

--- src/main/java/client/Hello.java
+++ src/main/java/client/Hello.java
@@ -28,94 +28,134 @@ import sun.net.www.protocol.http.HttpURLConnection;
 public class Hello {
 
 	static final Logger logger = LoggerFactory.getLogger(Hello.class);
-	
+
 	private final String clientStorePath = "src/main/resources/client.jks";
 	private final String clientTrustStorePath = "src/main/resources/clienttrust.jks";
 	private final char[] storePass = "password".toCharArray();
 	private final char[] keyPass = "password".toCharArray();
 
-	
-	
-	public String hello(String address) throws NoSuchAlgorithmException, KeyStoreException,
-			CertificateException, IOException, UnrecoverableKeyException,
-			KeyManagementException {
+	public String hello(String address) throws NoSuchAlgorithmException,
+			KeyStoreException, CertificateException, IOException,
+			UnrecoverableKeyException, KeyManagementException {
 		URL url = new URL(address);
 
-		HttpsURLConnection con = getConnection(url);
-		String response = getResponse(con);
+		HttpsURLConnection con = null;
+		String response = null;
+		try {
+			getConnection(url);
+			response = getResponse(con);
+		} finally {
+			if (con != null) {
+				con.disconnect();
+			}
+		}
 		logger.info(response);
 		return response;
 	}
-	
-	private HttpsURLConnection getConnection(URL url) throws IOException, NoSuchAlgorithmException, KeyStoreException, CertificateException, UnrecoverableKeyException, KeyManagementException{
+
+	private HttpsURLConnection getConnection(URL url) throws IOException,
+			NoSuchAlgorithmException, KeyStoreException, CertificateException,
+			UnrecoverableKeyException, KeyManagementException {
 		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
 		con.setRequestMethod("GET");
 
 		SSLContext sslContext = SSLContext.getInstance("TLS");
-		
+
 		KeyStore clientStore = getKeyStore(clientStorePath, storePass);
-		KeyManagerFactory clientKeyManager = getKeyManagerFactory(clientStore, keyPass);
-		
+		KeyManagerFactory clientKeyManager = getKeyManagerFactory(clientStore,
+				keyPass);
+
 		KeyStore clientTrustStore = getKeyStore(clientTrustStorePath, storePass);
-		TrustManagerFactory clientTrustKeyManager = getTrustManagerFactory(clientTrustStore, keyPass);
-		
+		TrustManagerFactory clientTrustKeyManager = getTrustManagerFactory(
+				clientTrustStore, keyPass);
+
 		con.setHostnameVerifier(getHostnameVerifier());
 
-		sslContext.init(clientKeyManager.getKeyManagers(), clientTrustKeyManager.getTrustManagers(), null);
+		sslContext.init(clientKeyManager.getKeyManagers(),
+				clientTrustKeyManager.getTrustManagers(), null);
 		con.setSSLSocketFactory(sslContext.getSocketFactory());
 		return con;
 	}
-	
-	private KeyManagerFactory getKeyManagerFactory(KeyStore keyStore, char[] password) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException{
+
+	private KeyManagerFactory getKeyManagerFactory(KeyStore keyStore,
+			char[] password) throws KeyStoreException,
+			NoSuchAlgorithmException, CertificateException, IOException,
+			UnrecoverableKeyException {
 		KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
 		kmf.init(keyStore, password);
 		return kmf;
 	}
-	
-	private TrustManagerFactory getTrustManagerFactory(KeyStore keyStore, char[] password) throws KeyStoreException, NoSuchAlgorithmException{
+
+	private TrustManagerFactory getTrustManagerFactory(KeyStore keyStore,
+			char[] password) throws KeyStoreException, NoSuchAlgorithmException {
 		TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
 		tmf.init(keyStore);
 		return tmf;
 	}
-	
-	private KeyStore getKeyStore(String pathToStore,char[] password) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException{
+
+	private KeyStore getKeyStore(String pathToStore, char[] password)
+			throws KeyStoreException, NoSuchAlgorithmException,
+			CertificateException, IOException {
 		KeyStore ks = KeyStore.getInstance("JKS");
-		FileInputStream keyStore = new FileInputStream(pathToStore);
-		ks.load(keyStore, password);
+		FileInputStream keyStore = null;
+		try {
+			keyStore = new FileInputStream(pathToStore);
+			ks.load(keyStore, password);
+		} finally {
+			if (keyStore != null) {
+				keyStore.close();
+			}
+		}
 		return ks;
 	}
-	
-	private HostnameVerifier getHostnameVerifier(){
+
+	private HostnameVerifier getHostnameVerifier() {
 		return new HostnameVerifier() {
 			public boolean verify(String s, SSLSession sslSession) {
 				return s.equals(sslSession.getPeerHost());
 			}
 		};
 	}
-	
-	private String getResponse(HttpsURLConnection con) throws IOException{
 
-		int responseCode = con.getResponseCode();
+	private String getResponse(HttpsURLConnection con) throws IOException {
+
+		int responseCode;
+		try {
+			responseCode = con.getResponseCode();
+		} catch (IOException e) {
+			logger.error("Get response code", e);
+			return null;
+		}
 		InputStream inputStream;
 		if (responseCode == HttpURLConnection.HTTP_OK) {
-			inputStream = con.getInputStream();
-			System.out.println("Connection OK");
+			try {
+				inputStream = con.getInputStream();
+			} catch (IOException e) {
+				logger.error("Get response input stream", e);
+				return null;
+			}
 		} else {
 			inputStream = con.getErrorStream();
-			System.out.println("Connection error");
 		}
 
 		// Process the response
-		BufferedReader reader;
+		BufferedReader reader = null;
 		StringBuilder reslt = new StringBuilder();
 		String line = null;
-		reader = new BufferedReader(new InputStreamReader(inputStream));
-		while ((line = reader.readLine()) != null) {
-			reslt.append(line);
+		try {
+			reader = new BufferedReader(new InputStreamReader(inputStream));
+			while ((line = reader.readLine()) != null) {
+				reslt.append(line);
+			}
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+			if (inputStream != null) {
+				inputStream.close();
+			}
 		}
-
-		inputStream.close();
 		return reslt.toString();
 	}
-	
+
 }
